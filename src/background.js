794b87b7a954d877d054a3fbfa308dd87be42e7c
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-05-02 23:57:19 +0200

    Changed license to GNU General Public License 3 (applies retroactively)
    NOTICE: This change is applied retroactively to all versions.

--- src/background.js
+++ src/background.js
@@ -1,17 +1,18 @@
 /*
- * Copyright 2017 ExE Boss.
+ * Copyright (C) 2017 ExE Boss
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* global browser */
 const ABOUT_PAGES = [];
@@ -63,7 +64,7 @@ initPage("about:welcomeback",	"emoticon-smile",	true);
  * @returns {undefined}
  */
 function initPage(page, icon, privileged) {
-	registerPage({page: page, icon: icon, privileged: privileged}, (registered) => {
+	registerPage({page: page, icon: icon, privileged: privileged}, registered => {
 		if (!registered) {
 			console.warn("[Firefox about:about Button]", "Failed to register page:", page);
 		}
@@ -109,7 +110,7 @@ function registerPage(message, resolve, privileged) {
 		});
 		browser.runtime.sendMessage({
 			type: "pagesChanged"
-		}).catch((error) => {
+		}).catch(error => {
 			if (!error.message.startsWith("Could not establish connection. Receiving end does not exist."))
 				console.error(error);
 		});
@@ -128,11 +129,11 @@ browser.runtime.onMessage.addListener((message, sender, resolve) => {
 		} case "getPages": {
 			return browser.storage.local.get({
 				showDisabledButtons: false
-			}).then((settings) => {
+			}).then(settings => {
 				return settings.showDisabledButtons;
-			}).catch((error) => {
+			}).catch(error => {
 				console.warn(error);
-				return true;
+				return false;
 			}).then(showDisabledButtons => {
 				return {
 					pages: ABOUT_PAGES.slice(),
