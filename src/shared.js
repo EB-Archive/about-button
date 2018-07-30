b55c1ace35a1492d796b81fdfd6b3bfc611deee3
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2018-07-30 16:45:00 +0200

    Use gulp to dynamically build the vendors README.md file

--- src/shared.js
+++ src/shared.js
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/// <reference path="./types.d.ts"/>
 "use strict";
 /* eslint-disable no-unused-vars */
 
